a8a6101385915abceae11dc3cc0dfb6150d6bc96
Gavin Robertson <gavinrob92@gmail.com>
2019-11-09 16:45:34 -0700

    removed some eslintrc errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,19 @@
 module.exports = {
-	"env": {
-		"browser": true,
-		"commonjs": true,
-		"es6": true
-	},
-	"extends": "eslint:recommended",
-	"globals": {
-		"Atomics": "readonly",
-		"SharedArrayBuffer": "readonly"
-	},
-	"parserOptions": {
-		"ecmaVersion": 2018
-	},
-	"rules": {
-		"indent": [
-			"error",
-			"tab"
-		],
-		"linebreak-style": [
-			"error",
-			"unix"
-		],
-		"quotes": [
-			"error",
-			"double"
-		],
-		"semi": [
-			"error",
-			"always"
-		]
-	}
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true
+  },
+  extends: "eslint:recommended",
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaVersion: 2018
+  },
+  rules: {
+    quotes: ["error", "double"],
+    semi: ["error", "always"]
+  }
 };
