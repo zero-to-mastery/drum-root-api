873f3e5c37ae7a8e8788b884d3638e3e35d5402e
Aladdin Sonni <aladdin.sonni@gmail.com>
2019-11-26 10:50:44 +0200

    Remove PORT var from config.example.env file

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.all("*", (req, res, next) => {
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
