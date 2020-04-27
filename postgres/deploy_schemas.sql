952320e1be1a937b98ecd563b4d795a9b0bf81f0
RedJanvier <janvierntwali@gmail.com>
2020-04-27 18:32:16 +0200

    feature(database): added seeds
    added database users seeds for docker postgres
    modified users table for userId to be automatically incremented

--- postgres/deploy_schemas.sql
+++ postgres/deploy_schemas.sql
@@ -1,1 +1,3 @@
 \i '/docker-entrypoint-initdb.d/tables/users.sql'
+
+\i '/docker-entrypoint-initdb.d/seeds/seed.sql'
---
+++ postgres/seeds/seed.sql
@@ -0,0 +1,9 @@
+INSERT INTO users (
+    firstname, 
+    lastname, 
+    emailid
+) VALUES 
+('Jessy', 'Hoan', 'jessyh@gmail.com'),
+('Tim', 'Timson', 'ttim@gmail.com'),
+('John', 'Doe', 'johndoe@gmail.com'),
+('Nina', 'Mclarrson', 'mcnina@gmail.com');
